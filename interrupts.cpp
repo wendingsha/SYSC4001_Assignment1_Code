c779aa06cc3213fd6cc83dd339515a0fd770f98f
wendingsha <wendingsha@cmail.carleton.ca>
2025-09-29 21:48:52 -0400

    upload the whole code

--- interrupts.cpp
+++ interrupts.cpp
@@ -40,7 +40,48 @@ int main(int argc, char** argv) {
             execution += std::to_string(current_time) + "," + std::to_string(duration_intr) + ", CPU executing\n";
             current_time += duration_intr;
         }
+        else if (activity == "SYSCALL" || activity == "END_IO"){
+            int deviceNum = duration_intr;
 
+            if (deviceNum < 0 || deviceNum >= delays.size() || deviceNum >= vectors.size()){
+                execution += std::to_string(current_time) + "," + std::to_string(0) + ", WarningL invalid device number " + std::to_string(deviceNum) + "int trace.txt\n";
+                continue;
+            }
+
+            auto [exeCute, time_after_exeCute] = intr_boilerplate(current_time, deviceNum, context_save_time, vectors);
+            execution += exeCute;
+            current_time = time_after_exeCute;
+
+            int reamaining = delays[deviceNum];
+            while (reamaining > 0)
+            {
+                int temp = (reamaining >= execute_ISR_time ) ? execute_ISR_time :reamaining;
+                execution += std::to_string(current_time) + "," + std::to_string(temp) + ", execute ISR activity\n";
+                current_time += temp;
+                reamaining -= temp;
+
+            }
+
+
+            execution += std::to_string(current_time) + "," + std::to_string(iret_time) + ", IRET\n";
+            current_time += iret_time;
+            
+            execution += std::to_string(current_time) + "," + std::to_string(context_save_time) + ", context restored\n";
+            current_time += context_save_time;
+
+            execution += std::to_string(current_time) + ", " + std::to_string(switch_kernel_time) + ", switch to user model\n";
+            current_time += switch_kernel_time;
+
+            if (activity == "END_IO"){
+                execution += std::to_string(current_time) + ", " + std::to_string(delays[deviceNum]) + ": interrupt\n";
+
+            }
+        
+
+        }
+        else{
+            execution += std::to_string(current_time) + ", 0, Unknown activity: " + activity + "\n";
+        }
 
         /************************************************************************/
 
